1b1e58edb1c5c881d8c284d65d7ce623ac63ac54
Oscar Castellanos <oscareliascastellanoscordero@gmail.com>
2025-12-07 09:38:33 -0600

    Integracion TanStack Query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,17 @@
-import { Stack } from 'expo-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Stack } from 'expo-router';
 
 const RootLayout = () => {
+  const queryClient = new QueryClient();
+  
   return (
-    <Stack 
-      screenOptions={{
-        headerShown: false
-      }}
-    />
+    <QueryClientProvider client={queryClient}>
+      <Stack 
+        screenOptions={{
+          headerShown: false
+        }}
+      />
+    </QueryClientProvider>
   )
 }
 
---
+++ presentation/hooks/useClashRoyale.ts
@@ -0,0 +1,14 @@
+import getCardAction from "@/core/actions/get-cards.action";
+import { useQuery } from "@tanstack/react-query";
+
+export const useClashRoyale = () => {
+  const cardsQuery = useQuery({
+    queryKey: ['clash-royale', 'card'],
+    queryFn: getCardAction,
+    staleTime: 1000 * 60 * 60 * 24,
+  });
+
+  return {
+    cardsQuery,
+  };
+};
